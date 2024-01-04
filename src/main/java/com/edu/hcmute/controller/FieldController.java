4483412bfed1f297b675e81147d8ccd1b368f126
Bùi Thanh Duy <dtb1742002@gmail.com>
2024-01-04 18:08:33 +0700

    done field API

--- src/main/java/com/edu/hcmute/controller/FieldController.java
+++ src/main/java/com/edu/hcmute/controller/FieldController.java
@@ -1,9 +1,11 @@
 package com.edu.hcmute.controller;
 
 
+import com.edu.hcmute.dto.OptionDTO;
 import com.edu.hcmute.response.ResponseData;
 import com.edu.hcmute.response.ServiceResponse;
 import com.edu.hcmute.service.FieldService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +31,28 @@ public class FieldController {
     @GetMapping("/{id}")
     public ResponseEntity<ResponseData> getOneField(@PathVariable("id") Integer id){
         ServiceResponse res = fieldService.getOne(id);
+        return ResponseEntity.status(res.getStatusCode())
+                .body(ResponseData.builder()
+                        .status(res.getStatus())
+                        .message(res.getMessage())
+                        .data(res.getData())
+                        .build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseData> createField(@RequestBody @Valid OptionDTO optionDTO){
+        ServiceResponse res = fieldService.create(optionDTO);
+        return ResponseEntity.status(res.getStatusCode())
+                .body(ResponseData.builder()
+                        .status(res.getStatus())
+                        .message(res.getMessage())
+                        .data(res.getData())
+                        .build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseData> updateField(@PathVariable("id") Integer id, @RequestBody @Valid OptionDTO optionDTO){
+        ServiceResponse res = fieldService.update(optionDTO, id);
         return ResponseEntity.status(res.getStatusCode())
                 .body(ResponseData.builder()
                         .status(res.getStatus())
--- src/main/java/com/edu/hcmute/mapper/FieldMapper.java
+++ src/main/java/com/edu/hcmute/mapper/FieldMapper.java
@@ -3,9 +3,17 @@ package com.edu.hcmute.mapper;
 
 import com.edu.hcmute.dto.OptionDTO;
 import com.edu.hcmute.entity.Field;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface FieldMapper {
     OptionDTO fieldToOptionDTO(Field field);
+
+    Field optionDTOToField(OptionDTO optionDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFieldFromOptionDTO(OptionDTO optionDTO,@MappingTarget Field field);
 }
