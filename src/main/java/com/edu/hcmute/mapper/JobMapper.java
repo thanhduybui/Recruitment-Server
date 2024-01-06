c55eadcc8a949224cbf18ae9447a0d293883097a
Bùi Thanh Duy <dtb1742002@gmail.com>
2024-01-06 23:25:47 +0700

    done get detail job and update job

--- src/main/java/com/edu/hcmute/mapper/JobMapper.java
+++ src/main/java/com/edu/hcmute/mapper/JobMapper.java
@@ -5,8 +5,7 @@ import com.edu.hcmute.dto.JobDTO;
 import com.edu.hcmute.dto.JobRequestBody;
 import com.edu.hcmute.entity.*;
 import com.edu.hcmute.response.ShortData;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {JobMapperHelper.class})
 public interface JobMapper {
@@ -35,4 +34,14 @@ public interface JobMapper {
 
     ShortData workModeToShortData(WorkMode workMode);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "positionId", target = "position")
+    @Mapping(source = "companyId", target = "company")
+    @Mapping(source = "salaryId", target = "salaryRange")
+    @Mapping(source = "fieldId", target = "field")
+    @Mapping(source = "majorId", target = "major")
+    @Mapping(source = "experienceId", target = "experienceRange")
+    @Mapping(source = "skillIds", target = "skills")
+    @Mapping(source = "workModeId", target = "workMode")
+    void updateJobFromJobRequestBody(JobRequestBody jobRequestBody, @MappingTarget Job job);
 }
--- src/main/java/com/edu/hcmute/service/JobService.java
+++ src/main/java/com/edu/hcmute/service/JobService.java
@@ -1,8 +1,11 @@
 package com.edu.hcmute.service;
 
 
+import com.edu.hcmute.dto.JobDTO;
 import com.edu.hcmute.dto.JobRequestBody;
 import com.edu.hcmute.entity.Job;
+import com.edu.hcmute.exception.ResourceNotFoundException;
+import com.edu.hcmute.exception.UndefinedException;
 import com.edu.hcmute.mapper.JobMapper;
 import com.edu.hcmute.repository.JobRepository;
 import com.edu.hcmute.response.ResponseDataStatus;
@@ -12,32 +15,70 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class JobService {
-    private final JobRepository jobRepository;
-    private final JobMapper jobMapper;
     private static final String CREATE_JOB_SUCCESS = "Tạo công việc thành công";
     private static final String CREATE_JOB_FAIL = "Tạo công việc thất bại";
+    private static final String UPDATE_JOB_SUCCESS = "Cập nhật công việc thành công";
+    private static final String UPDATE_JOB_FAIL = "Cập nhật công việc thất bại";
+    private static final String JOB_NOT_FOUND = "Không tìm thấy công việc";
+    private static final String GET_JOB_SUCCESS = "Lấy thông tin công việc thành công";
+    private static final String GET_JOB_FAIL = "Lấy thông tin công việc thất bại";
 
-    public ServiceResponse create(JobRequestBody jobRequestBody){
-       try{
-           Job newJob = jobMapper.jobRequestBodyToJob(jobRequestBody);
-           jobRepository.save(newJob);
-           return ServiceResponse.builder()
-                   .status(ResponseDataStatus.SUCCESS)
-                   .statusCode(HttpStatus.CREATED)
-                   .message(CREATE_JOB_SUCCESS)
-                   .build();
-       }catch (Exception e){
-           log.error(e.getMessage());
-          return ServiceResponse.builder()
-                  .status(ResponseDataStatus.ERROR)
-                  .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
-                  .message(CREATE_JOB_FAIL)
-                  .build();
-       }
+    private final JobRepository jobRepository;
+    private final JobMapper jobMapper;
+    public ServiceResponse create(JobRequestBody jobRequestBody) {
+        try {
+            Job newJob = jobMapper.jobRequestBodyToJob(jobRequestBody);
+            jobRepository.save(newJob);
+            return ServiceResponse.builder()
+                    .status(ResponseDataStatus.SUCCESS)
+                    .statusCode(HttpStatus.CREATED)
+                    .message(CREATE_JOB_SUCCESS)
+                    .build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new UndefinedException(CREATE_JOB_FAIL);
+        }
     }
 
+    public ServiceResponse update(Long id, JobRequestBody jobRequestBody) {
+        try{
+            Job job = jobRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(JOB_NOT_FOUND));
+
+            jobMapper.updateJobFromJobRequestBody(jobRequestBody, job);
+
+            this.jobRepository.save(job);
+
+            return ServiceResponse.builder()
+                    .status(ResponseDataStatus.SUCCESS)
+                    .statusCode(HttpStatus.OK)
+                    .message(UPDATE_JOB_SUCCESS)
+                    .build();
+        }catch (Exception e){
+            log.error(e.getMessage());
+            throw new UndefinedException(UPDATE_JOB_FAIL);
+        }
+    }
+
+    public ServiceResponse getOne(Long id) {
+        try {
+            Job job = jobRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException(JOB_NOT_FOUND));
+            JobDTO jobDTO = jobMapper.jobToJobDTO(job);
+            return ServiceResponse.builder()
+                    .status(ResponseDataStatus.SUCCESS)
+                    .statusCode(HttpStatus.OK)
+                    .message(GET_JOB_SUCCESS)
+                    .data(Map.of("job", jobDTO))
+                    .build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new UndefinedException(GET_JOB_FAIL);
+        }
+    }
 }
