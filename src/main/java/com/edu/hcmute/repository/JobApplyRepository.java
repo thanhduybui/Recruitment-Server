0ddbd02e32c35d34d75b9c68a0fce5fb984602b7
buithanhduy <dtb1742002@gmail.com>
2024-06-30 12:39:14 +0700

    update new database

--- src/main/java/com/edu/hcmute/repository/JobApplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edu.hcmute.repository;
-
-import com.edu.hcmute.entity.JobApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobApplyRepository extends JpaRepository<JobApplication, Long> {
-}
